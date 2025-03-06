88b6626f00a508ec79d211e0038c49b1c87f1ec2
santiagochimko <santiagochimko@gmail.com>
2025-03-06 13:04:05 -0300

    correccion de config

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,10 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
